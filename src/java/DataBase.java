14a336bf6ccf466b6482df8730c31db46fab797b
Yenesew <yenesewsileshi@gmail.com>
2023-01-14 06:26:48 -0800

    commitrd

---
+++ src/java/DataBase.java
@@ -0,0 +1,41 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author uba
+ */
+public class DataBase {
+  private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";  
+  private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";  
+  private static final String USERNAME = "JAVA2021"; 
+  private static final String PASSWORD = "1616";  
+  
+  public Connection conn_method()
+  {
+      Connection connection = null;
+      try{
+          Class.forName(DRIVER);
+          connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+              if(connection != null){
+                  System.out.println("Connected");
+              } else{
+                  System.out.println(" Not Connected");
+              }
+      } catch(Exception e){
+                System.out.println(e);
+      }
+      return connection;
+  }
+  public static void main(String []args){
+      DataBase obj = new DataBase();
+       System.out.println(obj.conn_method());
+
+  }
+}
